af26c8f2bdc2c363d0046cdfa6ccc78748c30ede
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-04-06 22:53:34 +0530

    New Nasate Javascript

---
+++ Hosting.js
@@ -0,0 +1,24 @@
+// getName();
+// console.log(x);
+// console.log(getName);
+
+// var x = 10;
+
+// function getName() {
+//   console.log("Namaste Javascript");
+// }
+
+// Both of function is undefined becasue they are variable in hosting
+
+var getName2 = function () {};
+
+var getName = () => {};
+
+// var x = 7;
+// function getName() {
+//   console.log(x);
+//   console.log("Namste Javascript");
+// }
+
+// getName();
+// console.log(getName);
